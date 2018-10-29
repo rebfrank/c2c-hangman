d61fd97d825e44a6e390a4182d1528c83587f85d
Rebecca Frank <rebeccaffrank@gmail.com>
2018-10-29 04:02:24 +0000

    Add function to draw the hangman

---
+++ hangman.py
@@ -0,0 +1,45 @@
+hangman_parts = [ "head", "left arm", "torso", "right arm", "left leg", "right leg" ]
+hangman_characters = {
+    "head" : "  O",
+    "left arm" : " /",
+    "torso" : "|",
+    "right arm" : "\\",
+    "left leg" : " /",
+    "right leg" : " \\"
+}
+hangman_newlines = [ "head", "right arm", "right leg" ]
+num_wrong_guesses_allowed = len(hangman_parts)
+words = [
+    "apple",
+    "butterfly",
+    "car",
+    "pajama",
+    "kayak",
+    "zigzag",
+    "zombie",
+    "oxygen",
+    "able",
+    "baby",
+    "lock",
+    "ornament",
+    "quality",
+    "liquid",
+    "suggestion",
+    "weather",
+    "twist"
+    ]
+
+def draw_hangman(num_wrong_guesses):
+    print(" _____")
+    print(" |   |")
+    print(" | ",end="")
+    num_newlines = 0
+    for i in range(num_wrong_guesses):
+        print(hangman_characters[hangman_parts[i]],end="")
+        if hangman_parts[i] in hangman_newlines:
+            print("\n | ",end="")
+            num_newlines = num_newlines + 1
+    for i in range(len(hangman_newlines) - num_newlines):
+        print("\n |",end="")
+    print("____\n\n",end="")
+
